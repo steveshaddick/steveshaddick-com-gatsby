33894ed326138dbadec2f77462efacfb8234de30
Steve Shaddick <steveshaddick@gmail.com>
2020-02-02 23:25:18 -0500

    wip 404 styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,15 +4,13 @@ import { createGlobalStyle } from "styled-components";
 import SEO from "@components/SEO";
 import PageContainer from "@components/PageContainer";
 
-import "@layouts/DefaultLayout/dark-styles.css";
-
 const GlobalStyle = createGlobalStyle`
   body {
-    background: rgb(50, 50, 50);
+    background: rgb(75, 75, 75);
     color: #fff;
     
     .PageContainer {
-      background: rgb(50, 50, 50);
+      background: rgb(75, 75, 75);
     }
   }
 `;
@@ -22,7 +20,6 @@ const GlobalStyle = createGlobalStyle`
  */
 const NotFoundPage = () => (
   <PageContainer>
-    <GlobalStyle />
     <SEO title="404: Not found" />
     <h1>NOTHING TO SEE HERE</h1>
     <p>You&apos;re looking for something that doesn&apos;t exist.</p>
